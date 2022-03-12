ba63014eecf09cf0bc3dc6c46aac540f19fa5234
MubarekHussen <mubahussen2014@gmail.com>
2022-03-12 17:12:40 +0300

    using_puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Using printf.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char str[];
+
+	str[] = "\"Programming is like building a multilingual puzzle,\n";
+	puts(str);
+
+	return (0);
+}

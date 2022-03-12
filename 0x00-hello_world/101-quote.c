8b3d5f1a6ada7d3190de17d284746db024270512
MubarekHussen <mubahussen2014@gmail.com>
2022-03-12 21:34:27 +0300

    using_write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - Displaying sizeof data types.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int len;
+
+	write(1, msg, 58);
+
+	return (0);
+}
+

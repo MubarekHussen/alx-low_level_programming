c3e2a0566b8cc442503c9729cc81329d65eb3bae
MubarekHussen <mubahussen2014@gmail.com>
2022-03-12 03:02:46 +0300

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 	char ch;
 
 	for (ch = '0'; ch <= '9'; ch++)
-        {
-                putchar(ch);
-        }
+	{
+		putchar(ch);
+	}
 	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);

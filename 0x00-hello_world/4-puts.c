ea28164383f0506b3f773f272e851dac3459e518
MubarekHussen <mubahussen2014@gmail.com>
2022-03-12 17:15:36 +0300

    using_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,7 @@
  */
 int main(void)
 {
-	char str[];
-
-	str[] = "\"Programming is like building a multilingual puzzle,\n";
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle,\n");
 
 	return (0);
 }

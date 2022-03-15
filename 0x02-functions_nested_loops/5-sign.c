031f1b590c93b5f0234cd4625f09e3bb5ffa35c9
MubarekHussen <mubahussen2014@gmail.com>
2022-03-15 17:28:05 +0300

    ckeck_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - cheks lowercase letters!.
+ *
+ *@c: input to be tested.
+ *
+ * Return: 0 on success
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+
+		return (1);
+		_putchar('+');
+	}
+	else if (c == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (c < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}

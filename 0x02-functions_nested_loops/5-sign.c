c441c17edbd5c00684d8c16bf87d0a3d63971bba
MubarekHussen <mubahussen2014@gmail.com>
2022-03-15 17:33:00 +0300

    ckeck_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,18 @@ int print_sign(int c)
 	if (c > 0)
 	{
 
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (c == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else if (c < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	return (0);
 }

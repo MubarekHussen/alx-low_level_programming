465feb1a304cde10145eafae6098d94fc84407c7
MubarekHussen <mubahussen2014@gmail.com>
2022-03-16 01:51:28 +0300

    counting

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	int x;
 
-	if (n > 98)
+	if (n >= 98)
 	{
 		for (x = n; x >= 98; x--)
 		{
@@ -23,7 +23,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else if (0 <= 98)
+	else if (n < 98)
 	{
 		for (x = n; x <= 98; x++)
 		{

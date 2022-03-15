947f8173b8c06e78eb86f13daf51a3599c4d99bc
MubarekHussen <mubahussen2014@gmail.com>
2022-03-15 15:31:08 +0300

    creating_header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,9 @@
-#ifndef main
-#define main
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions.
+ */
 	int _putchar(char c);
    
 #endif

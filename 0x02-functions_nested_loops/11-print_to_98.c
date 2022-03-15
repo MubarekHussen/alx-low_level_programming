da2568c17dc8d35a20ed86a9725654b56d4e1351
MubarekHussen <mubahussen2014@gmail.com>
2022-03-16 01:05:47 +0300

    addition

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - count from n upto 98.
+ *
+ * @n: where the count starts.
+ *
+ * Return: 0 on success
+ */
+void print_to_98(int n)
+{
+	for (ch = 'n'; ch <= '98'; ch++)
+	{
+		_putchar(ch);
+	}
+}

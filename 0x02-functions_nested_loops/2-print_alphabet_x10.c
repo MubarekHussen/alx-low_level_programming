b1cb012967c80798afbc4e7ddfc8fa7eeb0a9c73
MubarekHussen <mubahussen2014@gmail.com>
2022-03-15 16:18:49 +0300

    alaphabet_10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet - Lowercase alphabet Letters!.
+ * 
+ *main - prints 10x
+ *
+ * Return: 0 on success
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		print_alphabet();
+	return (0);
+}

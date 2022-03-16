2899949698d3a6ad722b82a907779dc673d083f3
MubarekHussen <mubahussen2014@gmail.com>
2022-03-16 22:35:31 +0300

    Testing_code

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions.
+ */
+void positive_or_negative(int i);
+#endif

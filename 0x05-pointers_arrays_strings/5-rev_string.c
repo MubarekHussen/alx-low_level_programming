3bbf17dd7bea5d39a26fba7f8b16e2e16a2f2cbc
MubarekHussen <mubahussen2014@gmail.com>
2022-03-22 01:22:05 +0300

    print_reverse_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,14 @@ void rev_string(char *s)
 	int i, len, temp;
 	char n_s[1000];
 
-	for (i = 0, len = 0; s[i] != '\0'; i++, len++)
+	for (i = 0, len = 0; s[i] != '\0'; ++i, ++len)
 		;
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; ++i)
 	{
 		n_s[i] = s[i];
 	}
 	temp = len -1;
-	for (i = 0; i < len; i++, temp++)
+	for (i = 0; i < len; ++i, ++temp)
 	{
 		s[i] = n_s[temp];
 	}

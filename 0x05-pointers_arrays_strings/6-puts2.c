6a52e05fd4dae745767061c584baa786278a72d3
MubarekHussen <mubahussen2014@gmail.com>
2022-03-22 01:33:46 +0300

    jump

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,9 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\n'; i = i + 2)
+	for (i = 0; str[i] != '\n'; i = i + 2)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

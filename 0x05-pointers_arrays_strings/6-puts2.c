93eb5d65357f3481d7d924b093c6e3b420a83747
MubarekHussen <mubahussen2014@gmail.com>
2022-03-22 01:31:52 +0300

    jump

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - jump one character.
+ *
+ *@str: input string.
+ */
+void puts2(char *str);
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\n'; i = i + 2)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

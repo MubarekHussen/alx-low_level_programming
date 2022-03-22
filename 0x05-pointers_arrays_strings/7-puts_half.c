36ff9db5028d3cef859dea502014194305fc0e09
MubarekHussen <mubahussen2014@gmail.com>
2022-03-22 21:13:52 +0300

    half_the_string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - return length of string.
+ *
+ *@str: input string.
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int n = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	if (i % 2 != 0)
+		n = (i - 1) / 2;
+	while (n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}

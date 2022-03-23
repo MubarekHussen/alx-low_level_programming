22d8f7467f1ade46045b287799c116ab24885ab6
MubarekHussen <mubahussen2014@gmail.com>
2022-03-23 10:13:36 +0300

    string_concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - making string concati.
+ *@dest: main.
+ *@src: to be concatenated.
+ *Return: full string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*dest != '\0')
+	{
+		dest++;
+		++i;
+	}
+	while (src[i])
+	{
+		dest[i] = src[i];
+		++i;
+		++j;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

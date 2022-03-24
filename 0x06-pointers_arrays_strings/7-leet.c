9cd788ad1921c0d7a7581ce9c8d58b77d9560dd7
MubarekHussen <mubahussen2014@gmail.com>
2022-03-24 22:59:25 +0300

    leet_string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *a)
 	int i = 0, j = 0;
 
 	char leet_ch[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int leet_index[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	char leet_index[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 
 	for (i = 0; a[i]; i++)
 	{

121963e03c22eef3d7897d8dc05bd32d0bd400dd
MubarekHussen <mubahussen2014@gmail.com>
2022-03-28 18:14:43 +0300

    that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (s != NULL)
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
@@ -16,5 +16,7 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
+	if (*s == '\0')
+		return (s);
 	return (NULL);
 }

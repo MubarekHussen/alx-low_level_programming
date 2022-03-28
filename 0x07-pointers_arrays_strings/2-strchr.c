89ec8fed0350e5e9668d0dfb7e83aa6030923c8d
MubarekHussen <mubahussen2014@gmail.com>
2022-03-28 18:06:13 +0300

    that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strchr - a function that locates a character in a string.
- * @S: memory tobe filled.
- * @C:  check if the character is present.
+ * @s: memory tobe filled.
+ * @c:  check if the character is present.
  *
  * Return: a pointer when gets c.
  */

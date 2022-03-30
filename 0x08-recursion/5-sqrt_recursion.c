5a38887d8bf7fe46cc37a2c2a06742873633877b
MubarekHussen <mubahussen2014@gmail.com>
2022-03-30 23:50:19 +0300

    square_root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * sqrt - calculate sqrt.
+ *@n: input nummber.
+ *@j: start from 0.
+ *
+ *Return: square root.
+ */
+
+int sqrt(int n, int j)
+{
+	if (j < n/2)
+	{
+		if (j * j == n)
+		{
+			return (j);
+		}
+		return (sqrt(n,j + 1));
+	}
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - square root.
+ *@n: input nummber.
+ *
+ *Return: power.
+ */
+int _sqrt_recursion(int n)
+{
+	sqrt(n,0);
+}

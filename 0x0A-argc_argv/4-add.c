69f8e310b274ce5f090b2d3ca2345607216d9b45
MubarekHussen <mubahussen2014@gmail.com>
2022-04-02 13:20:25 +0300

    sum_all_arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,22 +11,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int sum = 0, i, j;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			for (j = 0; argv[i][j] != 0; j++)
+			if (isdigit(argv[i][j]) == 0)
 			{
-				if (isdigit(argv[i][j] == 0))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				puts("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[i]);
 		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);

1eb171cc4b756017ebcc74d6f7fd1ea274ce6c39
MubarekHussen <mubahussen2014@gmail.com>
2022-04-02 13:06:44 +0300

    sum_all_arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints sum of all arguments.
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 1.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc <= 1)
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j] == 0))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

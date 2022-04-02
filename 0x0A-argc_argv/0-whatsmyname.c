fa10cbd7b456e6d958ebaf124fef0972ff8f26d8
MubarekHussen <mubahussen2014@gmail.com>
2022-04-02 12:09:30 +0300

    What_is_my_name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - accept the argument.
+ *@argc: argument count.
+ *@argv: argument array.
+ *Return: nothing.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

7300b3513a26c7fd07ffc8697b1229a9b8282910
MubarekHussen <mubahussen2014@gmail.com>
2022-04-04 14:01:14 +0300

    create_dynamic_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars.
+ * @size: gives size of input.
+ * @c: iput chracter.
+ * Return: void
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ch_array;
+	int i;
+
+	ch_array = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		ch_array[i] = c;
+	}
+	if (size == 0)
+	{
+		return (0);
+	}
+	return ch_array;
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions.
+ */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif

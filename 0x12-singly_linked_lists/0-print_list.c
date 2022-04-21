065f81e085692d06c0504f4e67738b7c4c8fe80e
MubarekHussen <mubahussen2014@gmail.com>
2022-04-21 18:05:25 +0300

    singly_node_count

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - counts the number of nodes.
+ *
+ * @h: a pointer to the list.
+ * Return: num of node.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d]%s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+

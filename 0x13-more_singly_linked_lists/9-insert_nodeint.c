8b1b3c582bb3af5d08e4d2972e93224df885f148
MubarekHussen <mubahussen2014@gmail.com>
2022-04-27 00:00:41 +0300

    cleaning

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * sum_listint - sums all elements of the list.
+ * @head: to refer the head.
+ * Return: sum
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new_node, temp;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	temp = *head;
+	for (i = 0; temp->next == NULL && i < idx; ++i)
+	{
+		temp = temp->next;
+	}
+	if (i == idx)
+	{
+		new_node->n = n;
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
+	}
+	return (NULL);
+}

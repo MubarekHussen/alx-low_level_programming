91303b4551037a02157830d59de3fc817c32f298
MubarekHussen <mubahussen2014@gmail.com>
2022-04-27 01:00:42 +0300

    add_at_index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 
 /**
- * sum_listint - sums all elements of the list.
- * @head: to refer the head.
- * Return: sum
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: a pointer to a pointer to a list of structs of lintint_t.
+ * @idx: index where insertion should take place.
+ * @n: the number to be put in the node to be inserted.
+ * Return: adress of new node or NULL if fails.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -13,11 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new_node, *temp;
 
-	if (head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -25,16 +27,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 	temp = *head;
-	for (i = 0; temp->next == NULL && i < idx; ++i)
+	for (i = 0; temp != NULL && i < idx - 1; ++i)
 	{
 		temp = temp->next;
 	}
-	if (i == idx)
-	{
-		new_node->n = n;
-		new_node->next = temp->next;
-		temp->next = new_node;
-		return (new_node);
-	}
-	return (NULL);
+	if (temp == NULL)
+		return (NULL);
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }

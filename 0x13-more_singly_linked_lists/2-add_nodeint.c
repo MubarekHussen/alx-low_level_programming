539977031e12036bfdc8009a962f22bbd5cd1ce4
MubarekHussen <mubahussen2014@gmail.com>
2022-04-26 11:26:01 +0300

    add_node_at_first

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,15 +13,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	struct listint_s *new_node;
 
-	if (head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(struct listint_s));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	new_node->next = (*head);
 	*head = new_node;
-	if (new_node == NULL)
-	{
-		return (0);
-	}
 	return (new_node);
 }

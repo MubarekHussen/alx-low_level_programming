1410cee6867dc1439d199ab4e826a1b4be43d484
MubarekHussen <mubahussen2014@gmail.com>
2022-06-15 16:05:32 +0300

    advanced

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-def ispalindrome(n):
-    string = str(n)
-    return string == string[::-1]
+def palcheck(num):
+    number_string = str(num)
+    length = round(len(number_string)/2)
+    a = number_string[0:length]
+    b = number_string[length:]
+    b = b[::-1]
+    if a in b:
+        print(num)
+        print(a, b[::-1])
+        return 1
+    else:
+        return 0
 
-array = []
-for i in range(999, 99, -1):
-    for j in range(i, 99, -1):
-        if ispalindrome(i * j):
-            array.append(i * j)
-print(max(array))
+result = 0
+k = 0
+for i in range(999, 101, -1):
+    for j in range(999, 101, -1):
+        if palcheck(i * j) == 1 and i * j > result:
+            result = i * j
+            print('{:d} * {:d} = {:d}'.format(i, j, i * j), "is a palindrome")
+print(result)

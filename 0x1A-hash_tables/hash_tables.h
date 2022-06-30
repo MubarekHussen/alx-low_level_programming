025468d83b33fb65e5cac1febf81beab604eca2b
MubarekHussen <mubahussen2014@gmail.com>
2022-07-01 01:52:55 +0300

    trying

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,11 @@
-#ifndef _HASH_TABLES_H_
-#define _HASH_TABLES_H_
-
+#ifndef __HASH__H
+#define __HASH__H
+/***************************** Libraries ***********************/
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
+/****************** Structs *******************************/
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -34,6 +35,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/*********************** Function Prototypes *****************************/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -42,17 +44,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-/**
- * struct shash_node_s - Node of a sorted hash table
- *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- * @sprev: A pointer to the previous element of the sorted linked list
- * @snext: A pointer to the next element of the sorted linked list
- */
-typedef struct shash_node_s
-{
-	char *key;
-
+#endif
